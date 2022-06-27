cb6d1b6cbc4aa74450cdc15aae20672416f0a014
IvanHoliak <goliakivan@gmail.com>
2022-06-27 16:16:41 +0300

    Change port

--- server/config/index.ts
+++ server/config/index.ts
@@ -3,7 +3,7 @@ import { ICONFIG, T_DB_URL, T_PORT } from "../types";
 
 dotenv.config();
 
-const PORT: T_PORT = process.env.PORT || 4444;
+const PORT: T_PORT = process.env.PORT || 3333;
 const DB_URL: T_DB_URL = process.env.DB_URL || "mongodb+srv://rockwell:rockwell__322@cloud.tggqp.mongodb.net/?retryWrites=true&w=majority";
 
 export const CONFIG: ICONFIG = {
